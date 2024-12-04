ca8f4dfbe08fa5b7db8aeccd5393f6ef0d218bb2
genius-ayush <ayushsanjayrawal@gmail.com>
2024-12-04 12:56:44 +0530

    cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,6 +9,14 @@ const port: number = 3000;
 
 app.use(cors());
 
+app.use(cors(
+    {
+        origin: ["https://maskcraft.vercel.app/"] , 
+        methods : ["POST" , "GET"] , 
+        credentials : true 
+    }
+))
+
 app.use(express.json());
 app.use('/auth', authRoutes);
 
